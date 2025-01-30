67c778c14f5f2a22fd04992b4099b3ea321257b6
Pratik <bopsdlops>
2025-01-30 18:54:40 +0545

    fix error

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,17 +1,11 @@
 import express from "express";
-import {
-	login,
-	logout,
-	signup,
-	checkAuth,
-} from "../contollers/user.controller.js";
+import { login, signup, checkAuth } from "../contollers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
 
 router.get("/check-auth", verifyToken, checkAuth);
 
