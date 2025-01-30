0b71fdd5fb5a717a6942285588a84cb3be220f58
Pratik <bopsdlops>
2025-01-30 17:36:24 +0545

    fix error

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
 		httpOnly: true, // Prevent access to cookies via JavaScript
 		secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-		sameSite: "strict", // Prevent CSRF attacks
+		sameSite: "none", // Prevent CSRF attacks
 	});
 
 	return token;
